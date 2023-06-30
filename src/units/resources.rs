99fb08bd177fc336b69610ff4bb4755b250f06d9
Braeden Callahan <you@example.com>
2023-06-29 23:38:44 -0700

    changed selected unit to be an entity

--- src/units/resources.rs
+++ src/units/resources.rs
@@ -1,6 +1,4 @@
 use bevy::prelude::*;
 
-use super::components::Unit;
-
 #[derive(Resource)]
-pub struct SelectedUnit(pub Option<Unit>);
+pub struct SelectedUnit(pub Option<Entity>);
--- src/units/systems.rs
+++ src/units/systems.rs
@@ -5,7 +5,7 @@ use crate::{
         components::{HexTile, TileVariant},
         resources::HexColors,
     },
-    hexagon::{cursor_to_hex, hex_to_pixel, pixel_to_hex, Cube},
+    hexagon::{cursor_to_hex, hex_to_pixel, Cube},
 };
 
 use super::{components::Unit, resources::SelectedUnit};
@@ -29,7 +29,7 @@ pub fn check_for_unit_selection(
     windows: Query<&Window>,
     buttons: Res<Input<MouseButton>>,
     mut selected_unit: ResMut<SelectedUnit>,
-    units: Query<&Unit>,
+    units: Query<(&Unit, Entity)>,
 ) {
     let Some(hovered_hex) = cursor_to_hex(windows) else {
         return;
@@ -44,7 +44,7 @@ pub fn check_for_unit_selection(
         return;
     }
 
-    for unit in &units {
+    for (unit, entity) in &units {
         if unit.position.q != hovered_hex.q {
             continue;
         }
@@ -53,7 +53,7 @@ pub fn check_for_unit_selection(
             continue;
         }
 
-        selected_unit.0 = Some(unit.clone());
+        selected_unit.0 = Some(entity);
         return;
     }
 
@@ -62,15 +62,24 @@ pub fn check_for_unit_selection(
 
 pub fn highlight_unit_hex(
     selected_unit: Res<SelectedUnit>,
+    units: Query<&Unit>,
     mut hexes: Query<(&HexTile, &mut Handle<ColorMaterial>)>,
     colors: Res<HexColors>,
 ) {
-    let Some(selected_unit) = selected_unit.0.clone() else {
+    let Some(selected_entity) = selected_unit.0 else {
         return;
     };
 
-    let attack = selected_unit.absolute_attack_hexes();
-    let movement = selected_unit.absolute_move_hexes();
+    if units.get(selected_entity).is_err() {
+        return;
+    }
+
+    let Ok(unit) = units.get(selected_entity) else {
+        return;
+    };
+
+    let attack = unit.absolute_attack_hexes();
+    let movement = unit.absolute_move_hexes();
 
     for (hex, mut color_mat) in &mut hexes {
         *color_mat = match hex.variant {
